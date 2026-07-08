aadcf478aa233d5dbb32cd7c7b9297ffb9283c36
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-07-08 23:01:04 +0530

    Time: 178 ms (31.35%), Space: 80.8 MB (79.37%) - LeetHub

---
+++ 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool solve(vector<int>& counts, vector<int>& quantity, int ithCustomer){
+        //base case
+        if(ithCustomer == quantity.size()){
+            return true;
+        }
+
+        for(int i = 0; i < counts.size(); i++){
+            if(counts[i] >= quantity[ithCustomer]){
+                counts[i] -= quantity[ithCustomer];
+                if(solve(counts, quantity, ithCustomer + 1)){
+                    return true;
+                }
+                counts[i] += quantity[ithCustomer]; //backtraking
+            }
+        }
+
+        return false;
+    }
+    
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        unordered_map<int, int> countMap;
+
+        for(auto num: nums){
+            countMap[num]++;
+        }
+
+        vector<int> counts;
+        for(auto it: countMap){
+            counts.push_back(it.second);
+        }
+
+        sort(quantity.begin(), quantity.end(), greater<int>());
+
+        return solve(counts, quantity, 0);
+    }
+};
