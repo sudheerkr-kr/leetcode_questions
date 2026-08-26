54ce7eead20ec4ceed9da2e342c4f545216df1ac
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-08-26 23:13:03 +0530

    Time: 0 ms (100%), Space: 9 MB (18.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+
+        stack<char> st;
+
+        for(char ch:s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+
+                if(st.empty()){
+                    return false;
+                }
+                
+                if(ch == ')' && st.top() != '('){
+                    return false;
+                }
+                if(ch == '}' && st.top() != '{'){
+                    return false;
+                }
+                if(ch == ']' && st.top() != '['){
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+        
+        return st.empty() ? true : false; 
+    }
+};
