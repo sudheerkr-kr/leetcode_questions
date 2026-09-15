9c093bc1d04602ee5a923702cdc25bd9f09d7401
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-09-15 22:25:16 +0530

    Time: 0 ms (100%), Space: 11.8 MB (10.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,52 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       
-       if(t.size()>s.size()){
-        return "";
-       }
-       int freq[128]={0};
-       for(int i=0 ; i<t.size() ; i++){
-        freq[t[i]]++;
-       }
+        
+        if(t.size() > s.size()) {
+            return "";
+        }
 
-       int j=0; 
-       int count=0;
-       int minlen =INT_MAX;
-       int start=0;
+        int freqT[128] = {0};
+        int freqS[128] = {0};
 
-       for(int i=0; i<s.size() ; i++){
-            if(freq[s[i]]>0)
-            {
+        for(int i = 0; i < t.size(); i++) {
+            freqT[t[i]]++;
+        }
+
+        int j = 0;
+        int count = 0;
+        int minLen = INT_MAX;
+        int start = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            
+            freqS[s[i]]++;
+
+            if(freqS[s[i]] <= freqT[s[i]]) {
                 count++;
             }
-            freq[s[i]]--;
-            while(count==t.size())
-            {
-                if(i-j+1 <minlen){
-                    minlen=i-j+1;
-                    start=j;
+
+            while(count == t.size()) {
+                
+                if(i - j + 1 < minLen) {
+                    minLen = i - j + 1;
+                    start = j;
                 }
-                freq[s[j]]++;
-                if(freq[s[j]]> 0){
+
+                freqS[s[j]]--;
+
+                if(freqS[s[j]] < freqT[s[j]]) {
                     count--;
                 }
+
                 j++;
             }
+        }
 
-       } 
-       if(minlen == INT_MAX){
-        return "";
-       }
-       return s.substr(start,minlen);
+        if(minLen == INT_MAX) {
+            return "";
+        }
 
+        return s.substr(start, minLen);
     }
 };
